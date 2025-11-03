95ad6b56700299147c4f0a1782278dadef12dd93
Alexandre KIENTZ <alexkientz6@gmail.com>
2025-11-03 11:38:38 +0100

    mote exo

---
+++ TP1/tp1exo2.py
@@ -0,0 +1,10 @@
+x = 5
+y = 10
+
+x = x + 2
+y = y * 3
+x = x - 1
+y = y / 2
+
+print(x)
+print( y)
--- TP1/tp1exo3.py
+++ TP1/tp1exo3.py
@@ -1,5 +1,5 @@
-jour = 3
-heure = 9
-minute = 59
+jour = 1
+heure = 1
+minute = 1
 t = ((jour * 24) * 60) + (heure * 60) + minute
 print(t)
